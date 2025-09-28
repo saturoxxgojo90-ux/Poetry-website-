339719a032df6999826a64fb9e41506861c18f23
saturoxxgojo90-ux <saturoxxgojo90@gmail.com>
2025-09-28 12:36:03 +0500

    Update poet.js

--- poet.js
+++ poet.js
@@ -1,7 +1,7 @@
 const params = new URLSearchParams(window.location.search);
 const id = params.get("id");
 
-fetch("https://my-json-server.typicode.com/saturoxxgojo90-ux/Poetry-website-/db")
+fetch("https://raw.githubusercontent.com/saturoxxgojo90-ux/Poetry-website-/main/db.json")
   .then(res => res.json())
   .then(data => {
     const poet = data.poets.find(p => p.id == id);
@@ -11,7 +11,10 @@ fetch("https://my-json-server.typicode.com/saturoxxgojo90-ux/Poetry-website-/db"
       const quotesDiv = document.getElementById("quotes");
       poet.quotes.forEach(q => {
         const p = document.createElement("p");
-        p.innerHTML = "❝ " + q.replace(/\n/g, "<br>") + " ❞";
+
+        // 🔥 This makes \n turn into real new lines
+        p.innerHTML = "❝ " + q.split("\n").join("<br>") + " ❞";
+
         quotesDiv.appendChild(p);
       });
     }
