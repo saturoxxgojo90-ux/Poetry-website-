96810d2b9e7a2c546d3b1e6c6c74ded34b03274f
saturoxxgojo90-ux <saturoxxgojo90@gmail.com>
2025-09-29 20:18:30 +0500

    Update poet.js

--- poet.js
+++ poet.js
@@ -15,7 +15,8 @@ const poets = [
     quotes: [
       "Khudi ko kar buland itna ke har taqdeer se pehle\nKhuda bande se khud pooche, bata teri raza kya hai",
       "Sitaron se aage jahan aur bhi hain\nAbhi ishq ke imtihan aur bhi hain",
-      "Tu shaheen hai, parwaz hai kaam tera\nTere saamne aasman aur bhi hain"
+      "Tu shaheen hai, parwaz hai kaam tera\nTere saamne aasman aur bhi hain",
+      "Dil ameer tha, muqadar gareeb tha\nHam buhat ache the, bura naseeb tha"
     ]
   }
 ];
